be06935217c6b9e0123a02638809b017f8e678a2
Evgeniy Shtuforuk <regedik22@gmail.com>
2022-10-19 01:05:06 +0300

    Update for Ira

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+const heartCode = countEl.textContent;
+const heartsCode = '💕';
 
 function increment() {
     count += 1
-    countEl.textContent = count
+    countEl.textContent = count + heartCode
 }
 
 function save() {
-    let countStr = count + " - "
+    let countStr = count + heartsCode + " - "
     saveEl.textContent += countStr
-    countEl.textContent = 0
+    countEl.textContent = 0 + heartCode
     count = 0
 }
